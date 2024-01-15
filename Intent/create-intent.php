1d075b7a155ddf273d652aae069d4ba403999b79
Shalom Moise <s.moise@blinkpayment.co.uk>
2024-01-15 18:00:38 +0000

    error handling

--- Intent/create-intent.php
+++ Intent/create-intent.php
@@ -1,39 +1,38 @@
-<?php 
+<?php
 
 $tokenData = file_get_contents("../Token/token.json");
 $data = json_decode($tokenData);
 
 $headers = array(
     'Content-type: application/x-www-form-urlencoded',
-    'Authorization: Bearer ' . $data->access_token
+    "Authorization: Bearer $data->access_token"
 );
 
-$url = "https://". $data -> blink_env.".blinkpayment.co.uk/api/pay/v1/intents";
+$url = "https://" . $data->blink_env . ".blinkpayment.co.uk/api/pay/v1/intents";
 
+$postData = http_build_query($_POST);
 
-$options = array(
-    'http' => array(
-        'header'  => implode("\r\n", $headers) . "\r\n",
-        'method'  => 'POST',
-        'content' => http_build_query($_POST)
-    )
-);
+$ch = curl_init($url);
 
-$context  = stream_context_create($options);
-$result = file_get_contents($url, false, $context);
-if ($result === FALSE) { /* Handle error */ 
-echo "Error in token";
-var_dump($result);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+$result = curl_exec($ch);
+
+if (curl_errno($ch)) {
+    echo 'Error: ' . curl_error($ch);
 } else {
-
-
-$obj = json_decode($result);
-$id = $obj->id;
-file_put_contents("./intent.json", json_encode($obj));
-echo "<p> Intent has been made. It has beens saved on the server for <a href='./intent.json'> demo purposes</a>. <br>
-Go to <a href='../Payment/create-payment.php'  class='button'>here to load payment sheet and pay</a>
-Or Try <a href='../Gpay/?intentId=". $id ."&googleEnv=TEST' class='button' >Google Pay</a>
-</p>";
+    $obj = json_decode($result);
+    // $id = $obj->id;
+    file_put_contents("./intent.json", json_encode($obj));
+    echo "<p> Intent has been made. It has been saved on the server for <a href='./intent.json'> demo purposes</a>. <br>
+    Go to <a href='../Payment/create-payment.php'  class='button'>here to load payment sheet and pay</a>
+    </p>";
 }
+
+curl_close($ch);
+
 echo '<script src="../footer.js"></script>';
-?>
+
